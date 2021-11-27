0adedd34dbf392b5b4fb5f8b58f985db9d09956d
Francisco <francisco.puente@alumnos.uva.es>
2021-11-27 23:10:28 +0100

    persistencia

--- main.js
+++ main.js
@@ -9,24 +9,90 @@ var active = '';
 
 window.onload = function() {
 
-    var second = setInterval(function() {
-        calculateLeftTime();
-        if(active === 'work') {
-            calculateWorkTime();
-        } else if (active === 'sleep') {
-            calculateSleepTime();
-        } else if (active === 'food') {
-            calculateFoodTime();
-        } else if (active === 'free') {
-            calculateFreeTime();
+    var storageHistoric = localStorage.getItem('activityHistoric');
+
+    if (storageHistoric !== null) {
+        const historic = storageHistoric.split(';').map((value) => {
+            return {
+                active: value.split('-')[0],
+                date: parseInt(value.split('-')[1]),
+            };
+        });
+
+        let latestDate = 0;
+        for (let i = 0; i < historic.length; i++) {
+            const activity = historic[i];
+            if (latestDate !== 0) {
+                if(active === 'work') {
+                    workTime = workTime + ((activity.date - latestDate) / 1000);
+                } else if (active === 'sleep') {
+                    sleepTime = sleepTime + ((activity.date - latestDate) / 1000);
+                } else if (active === 'food') {
+                    foodTime = foodTime + ((activity.date - latestDate) / 1000);
+                } else if (active === 'free') {
+                    freeTime = freeTime + ((activity.date - latestDate) / 1000);
+                }
+            }
+            active = activity.active;
+            latestDate = activity.date;
         }
+
+        let now = new Date().getTime();
+        if(active === 'work') {
+            workTime = workTime + ((now - latestDate) / 1000);
+        } else if (active === 'sleep') {
+            sleepTime = sleepTime + ((now - latestDate) / 1000);
+        } else if (active === 'food') {
+            foodTime = foodTime + ((now - latestDate) / 1000);
+        } else if (active === 'free') {
+            freeTime = freeTime + ((now - latestDate) / 1000);
+        }
+        
+    }
+
+    var second = setInterval(function() {
+        if(active === 'work') {
+            workTime = workTime + 1;
+        } else if (active === 'sleep') {
+            sleepTime = sleepTime + 1;
+        } else if (active === 'food') {
+            foodTime = foodTime + 1;
+        } else if (active === 'free') {
+            freeTime = freeTime + 1;
+        }
+        calculateTimes();
     }, 1000);
 
 }
 
 const activity = {
-    date: new Date().getTime(),
+    date: 0,
     active: '',
+    toString: function() {
+        return this.active + '-' + this.date.toString();
+    }
+}
+
+function saveNewActivity() {
+    const newActivity = Object.create(activity);
+    newActivity.date = new Date().getTime();
+    newActivity.active = active;
+
+    let historic = localStorage.getItem('activityHistoric');
+    if (historic === null) {
+        historic = newActivity.toString();
+    } else {
+        historic = historic + ';' + newActivity.toString();
+    }
+    localStorage.setItem('activityHistoric', historic);
+}
+
+function calculateTimes() {
+    calculateWorkTime();
+    calculateLeftTime();
+    calculateSleepTime();
+    calculateFoodTime();
+    calculateFreeTime();
 }
 
 function calculateLeftTime() {
@@ -41,8 +107,6 @@ function calculateLeftTime() {
 }
 
 function calculateWorkTime() {
-    workTime = workTime + 1;
-
     let hours = Math.floor((workTime / (60 * 60)));
     let minutes = Math.floor((workTime % (60 * 60)) / (60));
     let seconds = Math.floor((workTime % (60)));
@@ -51,8 +115,6 @@ function calculateWorkTime() {
 }
 
 function calculateSleepTime() {
-    sleepTime = sleepTime + 1;
-
     let hours = Math.floor((sleepTime / (60 * 60)));
     let minutes = Math.floor((sleepTime % (60 * 60)) / (60));
     let seconds = Math.floor((sleepTime % (60)));
@@ -61,8 +123,6 @@ function calculateSleepTime() {
 }
 
 function calculateFoodTime() {
-    foodTime = foodTime + 1;
-
     let hours = Math.floor((foodTime / (60 * 60)));
     let minutes = Math.floor((foodTime % (60 * 60)) / (60));
     let seconds = Math.floor((foodTime % (60)));
@@ -71,8 +131,6 @@ function calculateFoodTime() {
 }
 
 function calculateFreeTime() {
-    freeTime = freeTime + 1;
-
     let hours = Math.floor((freeTime / (60 * 60)));
     let minutes = Math.floor((freeTime % (60 * 60)) / (60));
     let seconds = Math.floor((freeTime % (60)));
@@ -82,16 +140,20 @@ function calculateFreeTime() {
 
 function startWorkTime() {
     active = 'work';
+    saveNewActivity();
 }
 
 function startSleepTime() {
     active = 'sleep';
+    saveNewActivity();
 }
 
 function startFoodTime() {
     active = 'food';
+    saveNewActivity();
 }
 
 function startFreeTime() {
     active = 'free';
+    saveNewActivity();
 }
